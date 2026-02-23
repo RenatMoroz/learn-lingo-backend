29ca62e02c8b01a64f0fd2193d90d374828d38b0
Renat Moroz <bs4123555@gmail.com>
2026-02-23 19:13:04 +0100

    min fix

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -6,7 +6,12 @@ export function env(name: string, defaultValue?: string): string {
   const value = process.env[name];
 
   if (value !== undefined) return value;
+
   if (defaultValue !== undefined) return defaultValue;
+
+  if (name === 'COGNITO_USER_POOL_ID' || name === 'COGNITO_CLIENT_ID') {
+    return '';
+  }
 
   throw new Error(`Missing: process.env['${name}'].`);
 }
